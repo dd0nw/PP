f4d1d1de75d1aa424d3af3eac223bccf5ba83374
sfm <bom5928@gmail.com>
2024-07-30 16:21:07 +0900

    hi

--- Back/Server.js
+++ Back/Server.js
@@ -1,44 +1,20 @@
-process.env.ORA_SDTZ = 'Asia/Seoul';
-const express = require('express');
-const oracledb = require('oracledb');
+process.env.ORA_SDTZ = "Asia/Seoul";
+require("dotenv").config();
+const express = require("express");
+const oracledb = require("oracledb");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
 oracledb.autoCommit = true;
-oracledb.initOracleClient({ libDir: 'C:/Users/smhrd/Desktop/instantclient' });
-
-const dbConfig = {
-  user: 'cgi_24S_IoT3_1',
-  password: 'smhrd1',
-  connectString: 'project-db-cgi.smhrd.com:1524/xe'
-};
-
-async function connectToOracle() {
-  try {
-    const connection = await oracledb.getConnection(dbConfig);
-    console.log('Successfully connected to Oracle database');
-    await connection.execute(`ALTER SESSION SET TIME_ZONE='UTC'`);
-    return connection;
-  } catch (err) {
-    console.error('Connection failed: ', err);
-  }
-}
-
-app.get('/', async (req, res) => {
-  const connection = await connectToOracle();
-  if (connection) {
-    try {
-      const result = await connection.execute(`SELECT SYSDATE FROM DUAL`);
-      res.send(`Current date and time: ${result.rows[0]}`);
-      await connection.close();
-    } catch (err) {
-      res.status(500).send('Error executing query');
-      console.error('Error executing query: ', err);
-    }
-  } else {
-    res.status(500).send('Error connecting to database');
-  }
+oracledb.initOracleClient({
+  libDir: "C:/Users/smhrd/Desktop/instantclient_11_2",
 });
+
+const router = require("./routes/router");
+app.use(cors()); // cors
+app.use(express.json()); //json 요청 바디 파싱
+app.use("/", router);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
--- Back/config/db.js
+++ Back/config/db.js
@@ -1,50 +1,51 @@
 try {
-    process.env.ORA_SDTZ = 'Asia/Seoul';
-    const oracledb = require('oracledb');
-    oracledb.autoCommit = true;
-  
-    console.log('Initializing Oracle client...');
-    oracledb.initOracleClient({ libDir: 'C:/Users/smhrd/Desktop/instantclient' });
-  
-    const dbConfig = {
-      user: 'cgi_24S_IoT3_1', 
-      password: 'smhrd1', 
-      connectString: 'project-db-cgi.smhrd.com:1524/xe'
-    };
-  
-    async function connectToOracle() {
-      try {
-        console.log('Attempting to connect to the Oracle database...');
-        const connection = await oracledb.getConnection(dbConfig);
-        console.log('Successfully connected to Oracle database');
-  
-        await connection.execute(`ALTER SESSION SET TIME_ZONE='UTC'`);
-  
-        const result = await connection.execute(`SELECT SYSDATE FROM DUAL`);
-        console.log('Current date and time: ', result.rows[0]);
-  
-        return connection;
-  
-      } catch (err) {
-        console.error('Connection failed: ', err);
-      }
+  process.env.ORA_SDTZ = "Asia/Seoul";
+  const oracledb = require("oracledb");
+  oracledb.autoCommit = true;
+
+  console.log("Initializing Oracle client...");
+  oracledb.initOracleClient({
+    libDir: "C:/Users/smhrd/Desktop/instantclient_11_2",
+  });
+
+  const dbConfig = {
+    user: "cgi_24S_IoT3_1",
+    password: "smhrd1",
+    connectString: "project-db-cgi.smhrd.com:1524/xe",
+  };
+
+  async function connectToOracle() {
+    try {
+      console.log("Attempting to connect to the Oracle database...");
+      const connection = await oracledb.getConnection(dbConfig);
+      console.log("Successfully connected to Oracle database");
+
+      await connection.execute(`ALTER SESSION SET TIME_ZONE='UTC'`);
+
+      const result = await connection.execute(`SELECT SYSDATE FROM DUAL`);
+      console.log("Current date and time: ", result.rows[0]);
+
+      return connection;
+    } catch (err) {
+      console.error("Connection failed: ", err);
     }
-  
-    // 테스트 실행
-    console.log('Starting connection test...');
-    connectToOracle().then(connection => {
-      if (connection) {
-        console.log('Connection test completed successfully.');
-      }
-    }).catch(err => {
-      console.error('Error during connection test: ', err);
-    });
-  
-    module.exports = {
-      connectToOracle: connectToOracle
-    };
-  
-  } catch (err) {
-    console.error('Initialization failed: ', err);
   }
-  
+
+  // 테스트 실행
+  console.log("Starting connection test...");
+  connectToOracle()
+    .then((connection) => {
+      if (connection) {
+        console.log("Connection test completed successfully.");
+      }
+    })
+    .catch((err) => {
+      console.error("Error during connection test: ", err);
+    });
+
+  module.exports = {
+    connectToOracle: connectToOracle,
+  };
+} catch (err) {
+  console.error("Initialization failed: ", err);
+}
