f52f329bfc94751f8645bd7a799655bdc36482fa
sfm <bom5928@gmail.com>
2024-08-02 17:44:27 +0900

    업데이트는 post 그냥 조회는 get -> /memo

--- Back/routes/router.js
+++ Back/routes/router.js
@@ -2,19 +2,19 @@ const express = require("express");
 const router = express.Router();
 const userRoutes = require("./user");
 const analysisRoutes = require("./analysis");
-const connectToOracle = require("../config/db.js")
+const connectToOracle = require("../config/db.js");
 const memoRoutes = require("./memo");
 
 router.use("/user", userRoutes);
 router.use("/analysis", analysisRoutes);
-router.use("/memo", memoRoutes);
+router.use("/", memoRoutes);
 
 router.get("/", async (req, res) => {
   const connection = await connectToOracle();
   if (connection) {
     try {
       const result = await connection.execute(`SELECT SYSDATE FROM DUAL`);
-      console.log("연결")
+      console.log("연결");
       res.send(`Current date and time: ${result.rows[0]}`);
       await connection.close();
     } catch (err) {
